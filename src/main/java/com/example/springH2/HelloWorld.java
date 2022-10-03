1b516086cea6f38c1ba89211dc9fadc6c73c366c
Jagadeesh Royal <jagadeeshroyal@Jagadeeshs-MacBook-Air.local>
2022-10-03 21:37:14 +0530

    Updated pom.xml with Dev tool

--- src/main/java/com/example/springH2/HelloWorld.java
+++ src/main/java/com/example/springH2/HelloWorld.java
@@ -1,15 +1,12 @@
 package com.example.springH2;
 
-import java.util.Date;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
-	@RequestMapping(value = "/",method=RequestMethod.GET)
+	@GetMapping(value = "/")
 	public String helloMethod() {		
 		return "Jagadeesh Royal Started on Oct-03-2022";
 	}
